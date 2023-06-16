9694eec6ece5d10da16c4a97734b9f8cb681cdb8
unknown <gbluedev@gmail.com>
2023-06-16 12:49:26 +0200

    user ping cmd

---
+++ user.py
@@ -0,0 +1,26 @@
+import disnake
+from disnake import message
+from disnake.ext import commands
+from disnake.ext.commands import command, has_permissions, bot_has_permissions
+from disnake.ui import View, Button, button
+from disnake import ButtonStyle, Interaction
+from disnake.ext import tasks
+import random
+
+class UserCmds(commands.Cog):
+    client = commands
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'User Cog is online.')
+
+    
+    # ------------------------ Commands
+
+    @commands.slash_command()
+    async def ping(self, inter):
+        latency = round(self.client.latency * 1000)
+
+        await inter.response.send_message(f'Pong! Latency: {latency}ms')
