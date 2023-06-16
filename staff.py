8c4c0b48eb2b8ba1147b83de6e6ea13e6b41761f
unknown <gbluedev@gmail.com>
2023-06-16 14:50:05 +0200

    result v1

--- staff.py
+++ staff.py
@@ -1,51 +1,6 @@
 import disnake
 from disnake.ext import commands
-from disnake.ui import View, Button, Select
-
-class ResultModal(disnake.ui.View):
-    def __init__(self, inter, username, notes):
-        super().__init__()
-        self.inter = inter
-        self.username = username
-        self.notes = notes
-        
-        self.result = None  # Variable to store the selected result
-    
-    async def callback(self, interaction: disnake.Interaction):
-        if interaction.component.id == 'result_dropdown':
-            self.result = interaction.data['values'][0]
-    
-    @disnake.ui.button(label='Submit', style=disnake.ButtonStyle.primary)
-    async def submit_button(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        if self.result is None:
-            await interaction.response.send_message('Please select a result.', ephemeral=True)
-            return
-        
-        channel = self.inter.channel
-        embed = disnake.Embed(title='Staff Application Result', color=0xe4d96f)
-        
-        if self.result == 'Accepted':
-            embed.color = disnake.Color.green()
-            result_role_id = 1119234205738602537
-        else:
-            embed.color = disnake.Color.red()
-            result_role_id = 1119234212243984424
-        
-        guild = self.inter.guild
-        result_role = guild.get_role(result_role_id)
-        
-        embed.set_author(
-            name=f"@{self.inter.author}",
-            icon_url="https://cdn.discordapp.com/attachments/1115898779552456744/1119233504610373672/Namnlos.png"
-        )
-        
-        embed.add_field(name="Username:", value=self.username.mention, inline=False)
-        embed.add_field(name="Notes", value=self.notes, inline=False)
-        embed.add_field(name="Result:", value=result_role.mention, inline=False)
-        
-        await channel.send(embed=embed)
-        await interaction.response.send_message(":white_check_mark: **Sent it to the channel.**", ephemeral=True)
-
+from disnake.ui import SelectMenu, SelectOption
 
 class Staff(commands.Cog):
     def __init__(self, client):
@@ -57,19 +12,49 @@ class Staff(commands.Cog):
 
     @commands.slash_command()
     async def result(self, inter, username: disnake.Member, notes: str):
-        modal_view = ResultModal(inter, username, notes)
-        select = Select(
-            placeholder='Select result',
-            options=[
-                disnake.SelectOption(label='Accepted', value='Accepted'),
-                disnake.SelectOption(label='Denied', value='Denied')
-            ],
-            custom_id='result_dropdown',
-            max_values=1,
-            min_values=1
+        options = [
+            SelectOption(label='Accepted', value='Accepted'),
+            SelectOption(label='Denied', value='Denied')
+        ]
+        select_menu = SelectMenu(custom_id='result_dropdown', placeholder='Select result', options=options)
+        message = await inter.response.send_message(
+            'Select the result:',
+            view=disnake.ui.View(select_menu)
         )
-        modal_view.add_item(select)
-        await inter.response.send_message('Select the result:', view=modal_view)
+
+        def check(interaction: disnake.Interaction):
+            return interaction.message == message and interaction.user == inter.author
+
+        try:
+            interaction = await self.client.wait_for('select_option', check=check, timeout=60.0)
+            result = interaction.data['values'][0]
+
+            embed = disnake.Embed(title='Staff Application Result', color=0xe4d96f)
+            if result == 'Accepted':
+                embed.color = disnake.Color.green()
+                result_role_id = 1119234205738602537
+            else:
+                embed.color = disnake.Color.red()
+                result_role_id = 1119234212243984424
+
+            guild = inter.guild
+            result_role = guild.get_role(result_role_id)
+
+            embed.set_author(
+                name=f"@{inter.author}",
+                icon_url="https://cdn.discordapp.com/attachments/1115898779552456744/1119233504610373672/Namnlos.png"
+            )
+
+            embed.add_field(name="Username:", value=username.mention, inline=False)
+            embed.add_field(name="Notes", value=notes, inline=False)
+            embed.add_field(name="Result:", value=result_role.mention, inline=False)
+
+            channel = self.client.get_channel(1115706650100244580)
+            await channel.send(embed=embed)
+            await interaction.response.send_message(":white_check_mark: **Sent it to the channel.**", ephemeral=True)
+
+        except TimeoutError:
+            await inter.response.send_message(":x: **You took too long to respond. Please try again.**", ephemeral=True)
 
 
 def setup(client):
