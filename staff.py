d241075f3d23fee183593c7ea00d86c642d7daa1
unknown <gbluedev@gmail.com>
2023-07-30 13:21:43 +0200

    app results pro

--- staff.py
+++ staff.py
@@ -38,21 +38,19 @@ class Staff(commands.Cog):
             result_role_id = 1119234212243984424
 
         guild = inter.guild
-        result_role = guild.get_role(result_role_id)
-        embed.add_field(name="", value=result_role.mention, inline=False)
         embed.set_author(
         name=f"@{inter.author}",
         icon_url="https://cdn.discordapp.com/attachments/1115898779552456744/1119233504610373672/Namnlos.png")
         
         embed.add_field(name="**Result:**", value=result, inline=False)    
         embed.add_field(name="**Application Username:**", value=username.mention, inline=False)    
-        embed.add_field(name="**Notes**", value=notes, inline=False)
+        embed.add_field(name="**Notes:*", value=notes, inline=False)
 
         
         
         await channel.send(f"{username.mention}")
         await channel.send(embed=embed)
-        
+        await inter.response.send_message(":white_check_mark: **Sent it to <#1134394613885575269>.**", ephemeral=True)
         
 
 
