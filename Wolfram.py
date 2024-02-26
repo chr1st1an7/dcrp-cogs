8c1c49423c83fee9095c532dbfef90561bb2ae9a
ParanoidExtreme <138971752+ParanoidExtreme@users.noreply.github.com>
2024-02-26 10:23:24 +0100

    Create Wolfram.py

---
+++ Wolfram.py
@@ -0,0 +1,38 @@
+import disnake
+from disnake.ext import commands
+import re
+import requests
+import urllib.parse
+
+class Wolfram(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Wolfram cog is online.')
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+
+        if message.content.startswith('.ask'):
+            question = message.content[4:].strip()
+            answer = ask_question(question)
+            await message.reply(answer)
+
+def setup(bot):
+    bot.add_cog(Wolfram(bot))
+
+def ask_question(question):
+  app_id = 'PWV8R7-TQ4LPW73YJ'
+  encoded_question = urllib.parse.quote(question)
+  api_url = f'https://api.wolframalpha.com/v1/result?appid={app_id}&i={encoded_question}'
+  response = requests.get(api_url)
+  if response.status_code == 200:
+      answer = response.text
+      return answer
+  else:
+      answer = "Failed to retrieve the answer."
+      return answer
